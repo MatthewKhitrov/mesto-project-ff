585c0e0aa25df8487ef64e043ccb853e9a1fb279
Матвей Хитров <Khitrovmatvei@yandex.ru>
2024-02-28 21:46:27 +0600

    версия №1

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Пологие горы с редкими снежными шапками",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Не сокрытое лдём озеро на фоне заснеженого Челябинского леса",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Рексрусия в виде многоэтажных домой которые стоят один за одним",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Гора на фоне степи Камчатки с островками ягеля",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Ровная линия железной дороги которая разрезает лес на две части",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Скала вырастающая из ледяного покрова озера Байкал",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+/* Глобальные переменные */
+const container = document.querySelector(".content");
+const placesList = container.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+/* Функция создания карточки + обработчик клика */
+function cardCreate(name, link, alt, cardDel) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").alt = alt;
+
+  buttonDelete.addEventListener("click", cardDel);
+
+  return cardElement;
+}
+
+/* Функция удаления карточки */
+function deleteCard(event) {
+  const deleteItem = event.target.closest(".card");
+  deleteItem.remove();
+}
+
+/* Функция добавления карточки */
+initialCards.forEach((item) => {
+  const addElement = cardCreate(item.name, item.link, item.alt, deleteCard);
+  placesList.append(addElement);
+});
